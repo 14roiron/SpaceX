e7b3a8853d21bb923019e9adf552f825dd86a243
14roiron <yohann.roiron@mines-paristech.fr>
2016-02-24 21:25:58 +0100

    update graphic interface and prepare the fist tests

--- src/com/roiron/spaceproject/Main.java
+++ src/com/roiron/spaceproject/Main.java
@@ -1,16 +1,19 @@
 package com.roiron.spaceproject;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import com.roiron.spaceproject.graphic.CercleShape;
 import com.roiron.spaceproject.graphic.SpacePanel;
 
 public class Main extends JFrame{
 	
-	JPanel spacePanel;  //the main frame, where we draw the space
+	SpacePanel spacePanel;  //the main frame, where we draw the space
 	
 	
-	public Main ()
+	public Main () throws InterruptedException
 	{
 		spacePanel = new SpacePanel();
 		this.setTitle("Space Simulation");
@@ -20,15 +23,21 @@ public class Main extends JFrame{
 		this.setContentPane(spacePanel);
 		this.setVisible(true);
 		
-		try {
+		Thread.sleep(1000);	
+		CercleShape c= new CercleShape(spacePanel.getWidth()/2,spacePanel.getHeight()/2,0, Color.blue, spacePanel.getWidth()/4);
+		spacePanel.addElementListObject(c);
+		spacePanel.repaint();
+		Thread.sleep(1000);
+		c.setAccX(0.01);
+		while(true)
+		{
+			spacePanel.repaint();
 			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 		
+		
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		new Main();
 
 	}
