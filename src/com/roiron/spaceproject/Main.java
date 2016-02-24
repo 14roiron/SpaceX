5038c270522359f9b7dccb42f362d2149474130d
14roiron <yohann.roiron@mines-paristech.fr>
2016-02-24 21:14:34 +0100

    update graphic interface and prepare the fist tests

--- src/com/roiron/spaceproject/Main.java
+++ src/com/roiron/spaceproject/Main.java
@@ -1,10 +1,37 @@
 package com.roiron.spaceproject;
 
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+import com.roiron.spaceproject.graphic.SpacePanel;
+
+public class Main extends JFrame{
+	
+	JPanel spacePanel;  //the main frame, where we draw the space
+	
+	
+	public Main ()
+	{
+		spacePanel = new SpacePanel();
+		this.setTitle("Space Simulation");
+		this.setSize(spacePanel.getWidth(), spacePanel.getHeight());
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setContentPane(spacePanel);
+		this.setVisible(true);
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Main();
 
 	}
+	
 
 }
