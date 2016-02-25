862ede39db09a54d3902fb7afd192cb01f18178f
14roiron <yohann.roiron@mines-paristech.fr>
2016-02-25 15:54:22 +0100

    add reference

--- src/com/roiron/spaceproject/physic/PhysicUtility.java
+++ src/com/roiron/spaceproject/physic/PhysicUtility.java
@@ -3,25 +3,33 @@ package com.roiron.spaceproject.physic;
 import com.roiron.spaceproject.graphic.GraphicObject;
 
 class PhysicUtility {
-		public static double[] toPolar(double[] vect)
+	/**
+	 * Transform the absolute coordinate to a polar one in the reference system
+	 * @param vect state vector we want to convert
+	 * @param ref reference system where we want to be
+	 * @return the polar coordinate in the reference system
+	 */
+		public static double[] toPolar(double[] vect, double[] ref)
 		{
-			double radius=Math.sqrt(Math.pow(vect[0], 2)+Math.pow(vect[1], 2));
-			double theta = Math.atan2(vect[1], vect[0]);
+			double radius=Math.sqrt(Math.pow(vect[0]-ref[0], 2)+Math.pow(vect[1]-ref[1], 2));
+			double theta = Math.atan2(vect[1]-ref[1], vect[0]-ref[0]);
 			return new double[]{radius,theta};
 		}
-		public static double[] toCarthe(double[] vect)
+		public static double[] toCarthe(double[] vect,double[] ref)
 		{
-			double x=vect[0]*Math.cos(vect[1]);
-			double y = vect[0]*Math.sin(vect[1]);
+			double x=vect[0]*Math.cos(vect[1]+ref[1])+ref[0];
+			double y = vect[0]*Math.sin(vect[1]+ref[1])+ref[0];
 			return new double[]{x,y};
 		}
-		public static double[][] toGlobalPolar(double[][] globalState)
+		public static double[][] toGlobalPolar(double[][] globalState, double[][] reference)
 		{
-			return new double[][]{toPolar(globalState[0]),toPolar(globalState[1])};
+			return new double[][]{toPolar(globalState[0],reference[0]),
+				toPolar(globalState[1],reference[1])};
 		}
-		public static double[][] toGlobalCarther(double[][] globalState)
+		public static double[][] toGlobalCarther(double[][] globalState,double[][] reference)
 		{
-			return new double[][]{toCarthe(globalState[0]),toCarthe(globalState[1])};
+			return new double[][]{toCarthe(globalState[0],reference[0]),
+				toCarthe(globalState[1],reference[1])};
 		}
 
 }
