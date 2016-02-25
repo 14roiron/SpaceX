8ec390b9a8968ffd6088bf2ae92ff2e6fa29dc26
14roiron <yohann.roiron@mines-paristech.fr>
2016-02-25 08:37:39 +0100

    initialisation du main

--- src/com/roiron/spaceproject/Main.java
+++ src/com/roiron/spaceproject/Main.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import com.roiron.spaceproject.graphic.CercleShape;
+import com.roiron.spaceproject.graphic.RectangleShape;
 import com.roiron.spaceproject.graphic.SpacePanel;
 
 public class Main extends JFrame{
@@ -17,22 +18,28 @@ public class Main extends JFrame{
 	{
 		spacePanel = new SpacePanel();
 		this.setTitle("Space Simulation");
-		this.setSize(spacePanel.getWidth(), spacePanel.getHeight());
+		this.setSize(spacePanel.width, spacePanel.height);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setContentPane(spacePanel);
 		this.setVisible(true);
 		
 		Thread.sleep(1000);	
-		CercleShape c= new CercleShape(spacePanel.getWidth()/2,spacePanel.getHeight()/2,0, Color.blue, spacePanel.getWidth()/4);
+		double h= SpacePanel.height;
+		double w= SpacePanel.width;
+		CercleShape c= new CercleShape(w/2,h/2,0, Color.blue, w/8);
+		RectangleShape r = new RectangleShape(w/2-w/6, h/2-h/6, 0, Color.black, w/12, h/12);
 		spacePanel.addElementListObject(c);
+		spacePanel.addElementListObject(r);
 		spacePanel.repaint();
 		Thread.sleep(1000);
 		c.setAccX(0.01);
+		r.setAtheta(0.01);
 		while(true)
 		{
 			spacePanel.repaint();
-			Thread.sleep(1000);
+			Thread.sleep(10);
+			System.out.println(r.getVtheta());
 		}
 		
 		
